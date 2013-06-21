4cd0e5341c8eb41bb55ab609dbd3648aa98cf40b
Stardrad Yin <yin8086@gmail.com>
2013-06-21 23:51:35 +0800

    complete IMport

--- Pim2Import.py
+++ Pim2Import.py
@@ -13,6 +13,9 @@ tileW = 16
 tileH = 8
 folder = u'.'
 
+def pureName(curName):
+    return curName[curName.rindex('\\') + 1:]
+    
 def walk(adr):
     root,dirs,files = os.walk(adr).next()
     for name in files:
@@ -36,8 +39,25 @@ def searchPal(col, pal, palBase, colNum):
     palBuf = [( ord(pal[i]), ord(pal[i+1]), ord(pal[i+2]), ord(pal[i+3]) ) \
                 for i in xrange(4 * baseAddr, 4 * (baseAddr + colNum), 4)]
     
-    pal[4 * baseAddr:4 * (baseAddr + colNum)]
-    
+    if col in palBuf:
+        return palBuf.index(col)
+    else:
+        distanceList = []
+        for palCol in palBuf:
+            rMean = (col[0] * col[3] + palCol[0] * palCol[3]) / 510
+            rDiff = (col[0] * col[3] - palCol[0] * palCol[3]) / 255
+            gDiff = (col[1] * col[3] - palCol[1] * palCol[3]) / 255
+            bDiff = (col[2] * col[3] - palCol[2] * palCol[3]) / 255
+            aDiff = col[3] - palCol[3]
+            dist  = (510 + rMean) * (rDiff**2) + \
+                    1020 * (gDiff ** 2) + \
+                    (765 - rMean) * (bDiff**2) + \
+                    1530 * (aDiff**2)
+            distanceList.append(dist)
+            if dist == 0:
+                break
+        minDist = min(distanceList)
+        return distanceList.index(minDist)
 
 def toTile(tiles, tarW, tarH,\
              posX, posY, xOffset, yOffset, \
@@ -50,13 +70,16 @@ def toTile(tiles, tarW, tarH,\
         for picX in xrange(posX, posX + xOffset):
             
             getInd = searchPal(pixBuf[picY*tarW + picX], pal, palBase, mark + 1)
-                        
+          
             tileOut = picY / tileH * tileInW + picX / tileW
             tileIn  = picY % tileH * tileW + picX % tileW 
             
-            getInd = getInd << shift
-
-            tiles[tileOut][tileIn] = getInd
+            if mark == 0xf:
+                # H(igh) L(ow)  H0 + 0L(4) 0L + H0(0)
+                bitOrigin = ord(tiles[tileOut][tileIn]) &  ( 0xf << ((shift + 4) % 8) )
+                bitSet    = getInd << shift
+                getInd = bitOrigin + bitSet
+            tiles[tileOut][tileIn] = chr(getInd)
 
 
 def toPim2(fPtr, im, startAddr, layer):
@@ -79,23 +102,76 @@ def toPim2(fPtr, im, startAddr, layer):
     picAddr = startAddr + headerSize + 0x10
     palBaseAddr = picAddr + picSize
     
-    # fPtr.seek(picAddr)
-    # tileArray = [list(fPtr.read(tileW * tileH)) for i in xrange(tileInW * tileInH)]
-    tileArray = [[0 for i in xrange(tileW * tileH)] for j in xrange(tileInW * tileInH)]
+    fPtr.seek(picAddr)
+    tileArray = [list(fPtr.read(tileW * tileH)) for i in xrange(tileInW * tileInH)]
+    # tileArray = [['\x00' for i in xrange(tileW * tileH)] for j in xrange(tileInW * tileInH)]
     
     fPtr.seek(palBaseAddr)
     palBuf = fPtr.read(4 * palNum)
 
+    if palNum > 0x100:
+        mapAddr = startAddr + 0x40
+    
+        fPtr.seek(mapAddr + 4)
+        blockNum = struct.unpack('<I', fPtr.read(4))[0]
+        blockInfo = []
+        for i in xrange(blockNum):
+            bInfo = BlockInfo()
+            bInfo.posX = struct.unpack('<H', fPtr.read(2))[0]
+            bInfo.posY = struct.unpack('<H', fPtr.read(2))[0]
+            bInfo.xOffset = struct.unpack('<H', fPtr.read(2))[0]
+            bInfo.yOffset = struct.unpack('<H', fPtr.read(2))[0]
+            bInfo.pal16Index = struct.unpack('<H', fPtr.read(2))[0]
+            bInfo.info = fPtr.read(2)
+            bInfo.shift = ord(fPtr.read(1))
+            bInfo.mark = ord(fPtr.read(1))
+            blockInfo.append(bInfo)
+            
+        pixBuf = list(im.getdata())
+        if layer == 0: 
+            for blkInd in (bid for bid in xrange(blockNum) if blockInfo[bid].shift != 4):                                   
+        
+                toTile(tileArray, width, height,\
+                     blockInfo[blkInd].posX, blockInfo[blkInd].posY,\
+                     blockInfo[blkInd].xOffset, blockInfo[blkInd].yOffset,\
+                     pixBuf, palBuf,\
+                     blockInfo[blkInd].pal16Index,\
+                     blockInfo[blkInd].shift, blockInfo[blkInd].mark)  
+
+        else:
+            for blkInd in (bid for bid in xrange(blockNum) if blockInfo[bid].shift == 4):
+                        
+                toTile(tileArray, width, height,\
+                     blockInfo[blkInd].posX, blockInfo[blkInd].posY,\
+                     blockInfo[blkInd].xOffset, blockInfo[blkInd].yOffset,\
+                     pixBuf, palBuf,\
+                     blockInfo[blkInd].pal16Index,\
+                     blockInfo[blkInd].shift, blockInfo[blkInd].mark) 
+
+            
+    elif palNum == 0x100:
+        pixBuf = list(im.getdata())
+        
+        toTile(tileArray, width, height,\
+             0, 0,\
+             width, height,\
+             pixBuf, palBuf,\
+             0, 0, 0xff)  
+        
+    fPtr.seek(picAddr)        
+    for tile in tileArray:
+        fPtr.write(''.join(tile))
+    print 'Import to %s at %08x @ layer %d' % (pureName(fPtr.name), startAddr, layer)
 
 for curName in walk(u'.'):
     #with open(curName, 'rb') as fPtr:
-    fName = curName[curName.rindex('\\') + 1:]
+    fName = pureName(curName)
     oriName = fName[:-15]
     pos = int(fName[-14: -6], 16)
     layer = int(fName[-5: -4])
     if not os.path.isfile(os.path.join(folder, oriName)):
         continue
     im = Image.open(curName)
-    with open(os.path.join(folder, oriName)) as binPtr:
+    with open(os.path.join(folder, oriName), 'r+b') as binPtr:
             toPim2(binPtr, im, pos, layer)
-            
+
