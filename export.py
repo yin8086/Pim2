98225e136cbd5bbe33ad7ba57e169adfc7b1c873
Stardrad Yin <yin8086@gmail.com>
2013-07-04 17:43:58 +0800

    update export and import

--- export.py
+++ export.py
@@ -1,48 +1,136 @@
-import struct,fnmatch,codecs,os
+import struct,fnmatch,codecs,os,re
 
 
-def fSize(fn):
-    with open(fn, 'rb') as fr:       
-        fr.seek(0,2)
-        retL = fr.tell()
-    return retL
-    
 def walk(adr):
     #root,dirs,files = os.walk(adr).next()
     for root,dirs, files in os.walk(adr):
         for name in files:
-            yield os.path.join(root, name)
-for fn in walk('script'):
-    if fnmatch.fnmatch(fn,'*.txt'):
-        continue
-    print fn
-    fp = open(fn,'rb')
-    fs = fSize(fn)
-    fp.seek(0x8)
-    string_offset = struct.unpack('I',fp.read(4))[0]
-    fp.seek(string_offset)
+            if not fnmatch.fnmatch(name, '*.txt'):
+                yield os.path.join(root, name)
+                
+def readUntilZ(myf):
+    restr=[]
+    while True:
+        byteg=myf.read(1)
+        if byteg=='\x00':break
+        restr.append(byteg)
     
-    dest = codecs.open(fn+'.txt','wb','utf16')
-    i = 0
+    return ''.join(restr)
+
+def convertCtr(match):
+    return u'{{{0:02x}}}'.format(ord(match.group()))
+
+def parseStr(tar):
+    fdIdx = 0
     while 1:
-        if fp.tell() >= fs:
-            break
-        string = ''
-        while 1:
-            char = fp.read(1)
-            if char == '\x00':
-                break
-            else:
-                if char == '\x0a':
-                    string += '\x0d\x0a'
-                elif ord(char) < 32:
-                    string += '[%02x]'%ord(char)
+        fdIdx = tar.find(u'[', fdIdx)
+        if fdIdx != -1 :
+            pat = re.compile(u'g.*\[[0-9a-f]*\]')
+            if pat.search(tar[:fdIdx+4]):
+                fdIdx += 4
+            elif (fdIdx + 3) < len(tar):
+                if tar[fdIdx+3] == u']':
+                    try:
+                        tar = ''.join([tar[:fdIdx], '{' ,tar[fdIdx+1:fdIdx+3], '}' ,tar[fdIdx+4:]])
+                        fdIdx += 4
+                    except ValueError:
+                        fdIdx += 4
                 else:
-                    string += char
-        if string == '':
-            continue
-        strings = string.decode('utf8')
-        dest.write('#### %d ####\r\n%s\r\n\r\n'%(i,strings))
-        i += 1
-    dest.close()
-    fp.close()
+                    fdIdx += 1
+            else:
+                break        
+        else:
+            break
+    return tar
+
+def printB(rhs):
+    for mbyte in rhs:
+        print '%02x' % ord(mbyte),
+
+def getRealAdd(strOff, ptrBuf, start):
+    fdIdx = ptrBuf.find('\x08\x00' + struct.pack('<I', strOff), start)    
+    if fdIdx == -1:
+        return 0
+    else:
+        return fdIdx + 2
+
+def getdata(myf):
+    isfirst=True
+    ltxt=[]
+    for line in myf:
+        if line.startswith(u'####'):
+            if not isfirst:
+                #print ltxt
+                tmpStr = u''.join(ltxt)
+                #print repr(tmpStr[:-4])
+                yield tmpStr[:-4]
+            else:isfirst=False
+            ltxt=[]
+        else:
+            ltxt.append(line)
+
+    tmpStr = u''.join(ltxt)
+    #print repr(tmpStr[:-4])
+    yield tmpStr[:-4]
+
+ctrlChars = re.compile(ur'[\x00-\x09\x0b-\x0c\x0e-\x1f]') 
+       
+for curName in walk('script'):
+    fName = curName[curName.rindex('\\') + 1:]
+    print 'Start %s: ' % fName,
+    outList = []
+    chscrN = curName.replace('script\\', r'chscript\\')+'.txt'
+    tarStrList = []
+    
+    if os.path.isfile(chscrN):
+        with codecs.open(chscrN, 'rb', 'utf16') as fRef:
+            for unistr in getdata(fRef):
+                tarStrList.append( parseStr(unistr))
+
+        with open(curName, 'rb') as fSrc:
+            f1Start = struct.unpack('<I', fSrc.read(4))[0]
+            f1Len = struct.unpack('<I', fSrc.read(4))[0]
+            f2Start = struct.unpack('<I', fSrc.read(4))[0]
+            f2Len = struct.unpack('<I', fSrc.read(4))[0]
+            
+            fSrc.seek(f1Start)
+            ptrBuf = fSrc.read(f1Len)
+            
+            fSrc.seek(f2Start)
+             
+            searchAdd = 0
+            idx = 0
+            while fSrc.tell() < f2Start + f2Len:
+                
+                while fSrc.read(1) == '\x00':
+                    pass
+                if fSrc.tell() >= f2Start + f2Len: # not EOF
+                    break
+                fSrc.seek(-1,1)
+                
+                curOff = fSrc.tell() - f2Start            
+                curStr = readUntilZ(fSrc)
+                curTOff = getRealAdd(curOff, ptrBuf, searchAdd)
+                assert curTOff != 0, 'Error'
+                # curStr2 = ctrlChars.sub(convertCtr, curStr.replace('\x0a','\r\n').decode('utf8'))
+                if tarStrList[idx] == u'':
+                    curStr = ctrlChars.sub(convertCtr, curStr.replace('\x0a','\r\n').decode('utf8'))
+                else:
+                    curStr = ctrlChars.sub(convertCtr, tarStrList[idx])
+                idx += 1
+                outList.append([curTOff+f1Start, curStr])
+                searchAdd = curTOff + 4
+                    
+    
+        if len(outList) > 0:
+            with codecs.open(curName + '.txt', 'wb', 'utf16') as fOut:
+                for textAdd, textStr in outList:
+                    fOut.write('#### %08x ####\r\n%s\r\n\r\n' % (textAdd, textStr))
+                print 'export %d script' % len(outList)
+    
+        else:
+            print 'Pass 1'
+    else:
+        print 'Pass 2'
+            
+        
--- import.py
+++ import.py
@@ -18,53 +18,36 @@ def walk(adr):
 def getdata(myf, lineDeli):
     isfirst=True
     ltxt=[]
+    add = 0
     for line in myf:
         if line.startswith(u'####'):
             if not isfirst:
                 #print ltxt
                 tmpStr = u''.join(ltxt)
                 #print repr(tmpStr[:-4])
-                yield tmpStr[:-4].replace(u'\r\n',lineDeli)
+                yield add, tmpStr[:-4].replace(u'\r\n',lineDeli)
             else:isfirst=False
             ltxt=[]
+            add=int(line[5:13],16)   
         else:
             ltxt.append(line)
 
     tmpStr = u''.join(ltxt)
     #print repr(tmpStr[:-4])
-    yield tmpStr[:-4].replace(u'\r\n',lineDeli)                
+    yield add, tmpStr[:-4].replace(u'\r\n',lineDeli)                
 
-def convertCtr(tar):
-    fdIdx = 0
-    while 1:
-        fdIdx = tar.find(u'[', fdIdx)
-        if fdIdx != -1 :
-            pat = re.compile(u'g.*\[[0-9a-f]*\]')
-            if pat.search(tar[:fdIdx+4]):
-                fdIdx += 4
-            elif (fdIdx + 3) < len(tar):
-                if tar[fdIdx+3] == u']':
-                    try:
-                        code = int(tar[fdIdx+1:fdIdx+3], 16)
-                        tar = ''.join([tar[:fdIdx], chr(code), tar[fdIdx+4:]])
-                        fdIdx += 1
-                    except ValueError:
-                        fdIdx += 4
-                else:
-                    fdIdx += 1
-            else:
-                break        
-        else:
-            break
-    return tar
+def convertCtr(match):
+    return unichr(int(match.group()[1:3], 16))
+
+invCtrlChars = re.compile(ur'\{[0-9a-f]{2}\}') 
     
 for curName in walk('script'):
     fName = curName[curName.rindex('\\') + 1:]
     print 'Start %s: ' % fName,
     tarStrList = []
     with codecs.open(curName, 'rb', 'utf16') as fSrc:
-        for unistr in getdata(fSrc, u'\x0a'):
-            tarStrList.append( convertCtr(unistr).encode('utf8') )
+        for addr, unistr in getdata(fSrc, u'\x0a'):
+            tarStrList.append( [addr, invCtrlChars.sub(convertCtr, unistr).encode('utf8')] )
     
     if len(tarStrList) > 0 and os.path.isfile(curName[:-4]):
         with open(curName[:-4], 'rb') as fOut:
@@ -83,10 +66,15 @@ for curName in walk('script'):
             headStr = fOut.read(oldAddr)
 
         with open(curName[:-4]+'.bin', 'wb') as fOut:
-            finalStr = '\x00'.join(tarStrList)
-            fLen = len(finalStr) + preZero
-            
+
             fOut.write(headStr)
+            
+            isFirst = True
+            
+            for addr,unistr in tarStrList:
+                if isFirst:
+                    fOut.write(unistr)
+            
             fOut.write(finalStr)
             numAlign = 4 - (fOut.tell() & 3)
             fOut.write('\x00' * numAlign)
